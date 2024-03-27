298bb0c3f6c48773ac350c822a888a57e33d3f1f
남병현 <kmi5063@naver.com>
2024-03-27 18:49:38 +0900

    feat : 상세페이지, 맛집등록페이지, 아이콘 추가

---
+++ pages/main/board/boardInsert.vue
@@ -0,0 +1,206 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const image = ref('');
+const goInsert = () => {
+  alert('등록');
+}
+const onFileChange: (event: Event) => void = (event) => {
+  const file = (event.target as HTMLInputElement).files![0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    image.value = e.target!.result as string;
+  };
+
+  reader.readAsDataURL(file);
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <div class="tasty_info">
+        <div class="mainTitle">DeliMap 정보입력</div>
+        <div class="tasty_title">
+          <label for="insert_title">맛집 이름</label>
+          <input type="text" id="insert_title" class="insert_title" autocomplete="off" placeholder="맛집 이름을 입력해주세요">
+        </div>
+        <div class="tasty_address">
+          <label for="insert_address">맛집 주소</label>
+          <input type="text" id="insert_address" class="insert_address" autocomplete="off" placeholder="맛집 주소를 입력해주세요">
+        </div>
+        <div class="tasty_phone">
+          <label for="insert_phone">맛집 전화번호</label>
+          <input type="text" id="insert_phone" class="insert_phone" autocomplete="off" placeholder="맛집 전화번호를 입력해주세요">
+        </div>
+        <div class="tasty_time">
+          <label for="insert_time">영업시간</label>
+          <span>월요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 09:00 ~ 22:00">
+          <span>화요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 09:00 ~ 22:00">
+          <span>수요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 09:00 ~ 22:00">
+          <span>목요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 09:00 ~ 22:00">
+          <span>금요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 09:00 ~ 22:00">
+          <span>토요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 11:00 ~ 20:00">
+          <span>일요일 : </span><input type="text" id="insert_time" class="insert_time" autocomplete="off" placeholder="예) 휴무일">
+        </div>
+        <div class="tasty_file">
+          <label for="insert_file">사진<span>(대표이미지 1장을 입력해주세요)</span></label>
+          <input type="file" id="insert_file" class="insert_file drag-disable" accept="image/*" @change="onFileChange">
+          <img v-if="image" :src="image" alt="대표이미지" class="insert_image drag-disable">
+        </div>
+        <div class="tasty_num">
+          <label for="insert_num">사업자 번호</label>
+          <input type="text" id="insert_num" class="insert_num" autocomplete="off" placeholder="사업자 번호를 입력해주세요">
+        </div>
+        <div class="goInsert">
+          <button @click="goInsert" class="goInsert_btn">등록</button>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+}
+.wrapper {
+  border: 1px solid rgb(248, 179, 60);
+  border-radius: 20px;
+  width: 700px;
+  padding: 20px;
+}
+.mainTitle {
+  font-size: 20px;
+  font-weight: 600;
+  text-align: center;
+}
+.tasty_title {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_title label {
+  font-size: 17px;
+}
+.insert_title {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 3px;
+}
+.insert_title:focus {
+  outline: none ;
+}
+.tasty_address {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_address label {
+  font-size: 17px;
+}
+.insert_address {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 3px;
+}
+.insert_address:focus {
+  outline: none ;
+}
+.tasty_phone {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_phone label {
+  font-size: 17px;
+}
+.insert_phone {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 3px;
+}
+.insert_phone:focus {
+  outline: none ;
+}
+.tasty_time {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_time label {
+  font-size: 17px;
+  display: block;
+}
+.tasty_time span {
+  font-size: 14px;
+}
+.insert_time {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 80%;
+  padding: 3px;
+  font-size: 15px;
+}
+.insert_time::placeholder {
+  font-size: 15px;
+}
+.insert_time:focus {
+  outline: none ;
+}
+.tasty_file {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_file span {
+  color: gray;
+  font-size: 15px;
+}
+.tasty_file label {
+  font-size: 17px;
+}
+.insert_file {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 3px;
+}
+.insert_file:focus {
+  outline: none ;
+}
+.insert_image {
+  width: 100%;
+  height: 200px;
+}
+.tasty_num {
+  margin: 15px 15px 15px 20px;
+}
+.tasty_num label {
+  font-size: 17px;
+}
+.insert_num {
+  border: none;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  width: 100%;
+  padding: 3px;
+}
+.insert_num:focus {
+  outline: none ;
+}
+.goInsert {
+  text-align: center;
+  margin: 20px auto;
+}
+.goInsert_btn {
+  background-color: rgb(248, 179, 60);
+  font-size: 20px;
+  color: white;
+  border-radius: 20px;
+  width: 100px;
+  height: 40px;
+  margin-top: 30px;
+}
+/*드래그 금지*/
+.drag-disable {
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  user-select: none;
+  -moz-user-select:none;
+  -webkit-touch-callout: none;
+}
+</style>
--- pages/main/menu.vue
+++ pages/main/menu.vue
@@ -1,46 +1,51 @@
-<script setup>
+<script setup lang="ts">
 import login1 from './member/login1.vue'
+import Modal from "~/pages/main/board/modal.vue";
+import boardInsert from "./board/boardInsert.vue";
+import { ref } from 'vue';
 
+const boardModal = ref(false);
+const reviewModal = ref(false);
+const deli_content = ref('내용 여긴 식당내용');
+const showFavorite = ref(true);
+const showFavoriteRed = ref(false);
+
+const copyUrl =  () => {
+  const url: string = location.href;
+  try {
+    navigator.clipboard.writeText(url);
+    // 복사 성공 시 알림 표시
+    alert('URL이 복사되었습니다!');
+  } catch {
+    // 복사 실패 시 알림 표시
+    alert('URL 복사에 실패했습니다!');
+  }
+};
 
 const router = useRouter()
 const goLogin = () => {
   router.addRoute({ name: 'login1', path: '/login1', component: login1 });
   router.push('/login1')
 }
+const tastyInsert = () => {
+  router.addRoute({ name: 'boardInsert', path: '/boardInsert', component: boardInsert });
+  router.push('/boardInsert')
+}
 </script>
-<script>
-import Modal from "./board/modal.vue";
-import { defineComponent } from 'vue';
-export default defineComponent({
-  components: {
-    Modal,
-  },
-  data() {
-    return {
-      showModal: false,
-      deli_content: '내용내용 여긴 식당 내용 여긴 식당 내용내용내용 여긴 식당 내용내용내용 여긴 식당 내용내용내용 여긴 식당 내용내용내용 여긴 식당 내용',
-      showFavorite: true,
-      showFavoriteRed: false,
-    };
-  },
-});
-</script>
-
 <template>
     <div>
         <div class="menu-btns">
             <div class="btns-wrapper">
                 <button class="btn-border active">지도</button>
-                <button class="btn-border" @click="showModal = true">게시판</button>
+                <button class="btn-border" @click="boardModal = true">게시판</button>
                 <button class="btn-border">채팅</button>
                 <button class="btn-border">내정보수정</button>
-                <button class="btn-border">맛집추가</button>
+                <button class="btn-border" @click="tastyInsert">맛집추가</button>
                 <button class="btn-border" @click="goLogin">로그인(임시)</button>
             </div>
         </div>
     </div>
-  <div id="modal">
-    <Modal v-if="showModal" @close="showModal = false">
+    <Modal v-if="boardModal" @close="boardModal = false">
 
       <template v-slot:header>
         <label class="member_profile">
@@ -50,68 +55,72 @@ export default defineComponent({
       </template>
       <template v-slot:body>
         <div class="deli_info drag-disable">
-          <lable>
-            <span class="backIcon"><img src="@/pages/assets/icons8-back-64.png" alt="이전"></span>
-            <div class="deli-img">여기는 이미지 공간</div>
-            <span class="nextIcon"><img src="@/pages/assets/icons8-forward-64.png" alt="다음"></span>
-          </lable>
+          <label>
+            <span class="backIcon"><img src="../assets/icons8-back-64.png" alt="이전" title="이전"></span>
+            <span class="deli-img"></span>
+            <span class="nextIcon"><img src="../assets/icons8-forward-64.png" alt="다음" title="다음"></span>
+          </label>
         </div>
-        <div class="deli_body">
+        <div class=" deli_body">
           <div class="deli_content">{{ deli_content }}
             <a href="#" v-if="deli_content.length > 50">...더보기</a>
           </div>
           <div class="deli_tag">
-            <div class="deli_hashtag">#맛집 #고기 #분위기맛집</div>
+            <div class="deli_hashtag">#맛집 #고기 #분위기</div>
             <div class="deli_pin drag-disable">
-              <img src="@/pages/assets/icons8-mappin-40.png" alt="핀">
+              <img src="../assets/icons8-mappin-40.png" alt="핀">
               <span class="deli_pin_between">ㅡ</span>
-              <img src="@/pages/assets/icons8-mappin-40.png" alt="핀">
+              <img src="../assets/icons8-mappin-40.png" alt="핀">
               <span class="deli_pin_between">ㅡ</span>
-              <img src="@/pages/assets/icons8-mappin-40.png" alt="핀">
+              <img src="../assets/icons8-mappin-40.png" alt="핀">
             </div>
           </div>
           <div class="deli_favorite drag-disable">
-            <img src="@/pages/assets/icons8-favorite-25.png" v-show="showFavorite"
-                 @click="showFavorite = false; showFavoriteRed = true" alt="좋아요">
-            <img src="@/pages/assets/icons8-favorite_red-25.png" v-show="showFavoriteRed"
-                 @click="showFavorite = true; showFavoriteRed = false" alt="좋아요">
-          <div class="deli_share">
-            <img src="@/pages/assets/icons8-share-24.png" alt="공유">
-          </div>
+            <img src="../assets/icons8-favorite-24.png" v-show="showFavorite"
+                 @click="showFavorite = false; showFavoriteRed = true" alt="좋아요" title="좋아요">
+            <img src="../assets/icons8-favorite_red-24.png" v-show="showFavoriteRed"
+                 @click="showFavorite = true; showFavoriteRed = false" alt="좋아요" title="좋아요">
+            <div class="deli_share" @click="copyUrl">
+              <img src="../assets/icons8-share-24.png" alt="공유하기" title="공유하기">
+            </div>
+            <div class="deli_review">
+              <button class="deli_review_btn" @click="reviewModal = true; boardModal = false">리뷰 쓰기</button>
+            </div>
           </div>
           <div class="borderLine"/>
 
-            <div class="comment_body">
-              <div class="comment_title">방문자 리뷰</div>
-              <label class="comment_member_profile">
-                <span class="comment_member_profile-img"></span>
-                <span class="comment_member_nickname">닉네임</span>
-              </label>
-              <div class="comment_content">
-                댓글 내용 댓글 내용 댓글 내용 댓글 내용 댓글 내용
-              </div>
-              <label class="comment_member_profile">
-                <span class="comment_member_profile-img"></span>
-                <span class="comment_member_nickname">닉네임</span>
-              </label>
-              <div class="comment_content">
-                댓글 내용 댓글 내용 댓글 내용 댓글 내용 댓글 내용
-              </div>
-              <label class="comment_member_profile">
-                <span class="comment_member_profile-img"></span>
-                <span class="comment_member_nickname">닉네임</span>
-              </label>
-              <div class="comment_content">
-                댓글 내용 댓글 내용 댓글 내용 댓글 내용 댓글 내용
-              </div>
+          <div class="comment_body">
+            <div class="comment_title">방문자 리뷰</div>
+            <label class="comment_member_profile">
+              <span class="comment_member_profile-img"></span>
+              <span class="comment_member_nickname">단무지</span>
+            </label>
+            <div class="comment_content">
+              댓글 내용 댓글 내용 댓글 내용 댓글 내용 댓글 내용
             </div>
+            <label class="comment_member_profile">
+              <span class="comment_member_profile-img"></span>
+              <span class="comment_member_nickname">단무지</span>
+            </label>
+            <div class="comment_content">
+              댓글 내용 댓글 내용 댓글 내용 댓글 내용 댓글 내용
+            </div>
+          </div>
         </div>
-
       </template>
       <template v-slot:footer>
       </template>
     </Modal>
-  </div>
+
+  <Modal v-if="reviewModal" @close="reviewModal = false">
+    <template v-slot:header>
+      <label class="member_profile">
+        <span class="member_profile-img"></span>
+        <span class="member_nickname">단무지</span>
+      </label>
+    </template>
+  </Modal>
+
 </template>
 
 <style scoped>
@@ -141,6 +150,7 @@ export default defineComponent({
     padding:10px;
     border-radius: 10px;
 }
+
 /*----------------------------모 달 창--------------------------------------*/
 .member_profile {
   display: flex;
@@ -159,7 +169,7 @@ export default defineComponent({
 .member_nickname {
   margin-left: 20px;
 }
-.deli_info lable{
+.deli_info label{
   display: flex;
   align-items: center;
 }
@@ -185,6 +195,8 @@ export default defineComponent({
 }
 .deli_content {
   width: 80%;
+  margin: 10px auto 5px auto;
+  padding-left: 10px;
 }
 .deli_content a {
   color: grey;
@@ -216,6 +228,15 @@ export default defineComponent({
 }
 .deli_share {
   margin-left: 10px;
+}
+.deli_review {
+  display: flex;
+  justify-content: right;
+  margin-left: auto;
+}
+.deli_review_btn {
+
+
 }
 .borderLine {
   border: rgba(105, 105, 105, 0.5) 7px solid;
